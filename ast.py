4c0f6d1787d19540b4e88541d29f590263168ba2
Nick Korasidis <renelvon@gmail.com>
2014-06-11 19:30:16 +0300

    Realigned ast.py and parser.py

--- ast.py
+++ ast.py
@@ -23,25 +23,6 @@ class Program(DataNode):
     def __init__(self, list):
         self.list = list
 
-class TypeDef(Node):
-    def __init__(self, list):
-        self.list = list
-
-class TDef(Node):
-    def __init__(self, name, list):
-        self.name = name
-        self.list = list
-
-class Constr(Node):
-    def __init__(self, name, list=None):
-        self.name = name
-        self.list = list or []
-
-class Param(DataNode):
-    def __init__(self, name, type=None):
-        self.name = name
-        self.type = type
-
 class LetDef(Node):
     def __init__(self, list, isRec=False):
         self.list = list
@@ -58,65 +39,13 @@ class FunctionDef(Def):
         self.body = body
         self.type = type
 
-class VariableDef(Def):
+class Param(DataNode):
     def __init__(self, name, type=None):
         self.name = name
         self.type = type
 
-class ArrayVariableDef(VariableDef):
-    def __init__(self, name, dimensions, type=None):
-        self.name = name
-        self.dimensions = dimensions
-        self.type = type
-
 class Expression(DataNode):
     pass
-
-class BangExpression(Expression):
-    def __init__(self, expr):
-        self.expr = expr
-
-class ArrayExpression(Expression):
-    def __init__(self, name, list):
-        self.name = name
-        self.list = list
-
-class IconstExpression(Expression):
-    def __init__(self, value):
-        self.value = value
-
-class FconstExpression(Expression):
-    def __init__(self, value):
-        self.value = value
-
-class CconstExpression(Expression):
-    def __init__(self, value):
-        self.value = value
-
-class SconstExpression(Expression):
-    def __init__(self, value):
-        self.value = value
-
-class BconstExpression(Expression):
-    def __init__(self, value):
-        self.value = value
-
-class UconstExpression(Expression):
-    def __init__(self):
-        pass
-
-class GenidExpression(Expression):
-    def __init__(self, name):
-        self.name = name
-
-class ConidExpression(Expression):
-    def __init__(self, name):
-        self.name = name
-
-class UnaryExpression(Expression):
-    def __init__(self, operator, operand):
-        self.operator = operator
-        self.operand = operand
 
 class BinaryExpression(Expression):
     def __init__(self, leftOperand, operator, rightOperand):
@@ -124,34 +53,65 @@ class BinaryExpression(Expression):
         self.operator = operator
         self.rightOperand = rightOperand
 
-class GcallExpression(Expression):
-    def __init__(self, name, list):
-        self.name = name
-        self.list = list
+class UnaryExpression(Expression):
+    def __init__(self, operator, operand):
+        self.operator = operator
+        self.operand = operand
 
 class CcallExpression(Expression):
     def __init__(self, name, list):
         self.name = name
         self.list = list
 
-class DimExpression(Expression):
-    def __init__(self, name, dimension=1):
+class ArrayExpression(Expression):
+    def __init__(self, name, list):
         self.name = name
-        self.dimension = dimension
+        self.list = list
 
-class NewExpression(Expression):
-    def __init__(self, type):
-        self.type = type
+class BangExpression(Expression):
+    def __init__(self, expr):
+        self.expr = expr
+
+class BconstExpression(Expression):
+    def __init__(self, value):
+        self.value = value
+
+class CconstExpression(Expression):
+    def __init__(self, value):
+        self.value = value
+
+class ConidExpression(Expression):
+    def __init__(self, name):
+        self.name = name
+
+class FconstExpression(Expression):
+    def __init__(self, value):
+        self.value = value
+
+class GenidExpression(Expression):
+    def __init__(self, name):
+        self.name = name
+
+class IconstExpression(Expression):
+    def __init__(self, value):
+        self.value = value
+
+class SconstExpression(Expression):
+    def __init__(self, value):
+        self.value = value
+
+class UconstExpression(Expression):
+    def __init__(self):
+        pass
 
 class DeleteExpression(Expression):
     def __init__(self, expr):
         self.expr = expr
 
-class IfExpression(Expression):
-    def __init__(self, condition, thenExpr, elseExpr=None):
-        self.condition = condition
-        self.thenExpr = thenExpr
-        self.elseExpr = elseExpr
+class DimExpression(Expression):
+    def __init__(self, name, dimension=1):
+        self.name = name
+        self.dimension = dimension
 
 class ForExpression(Expression):
     def __init__(self, counter, startExpr, stopExpr, body, isDown=False):
@@ -161,20 +121,26 @@ class ForExpression(Expression):
         self.body = body
         self.isDown = isDown
 
-class WhileExpression(Expression):
-    def __init__(self, condition, body):
-        self.condition = condition
-        self.body = body
-
-class MatchExpression(Expression):
-    def __init__(self, expr, list):
-        self.expr = expr
+class GcallExpression(Expression):
+    def __init__(self, name, list):
+        self.name = name
         self.list = list
 
 class LetInExpression(Expression):
     def __init__(self, letdef, expr):
         self.letdef = letdef
         self.expr = expr
+
+class IfExpression(Expression):
+    def __init__(self, condition, thenExpr, elseExpr=None):
+        self.condition = condition
+        self.thenExpr = thenExpr
+        self.elseExpr = elseExpr
+
+class MatchExpression(Expression):
+    def __init__(self, expr, list):
+        self.expr = expr
+        self.list = list
 
 class Clause(Node):
     def __init__(self, pattern, expr):
@@ -186,11 +152,7 @@ class Pattern(Node):
         self.name = name
         self.list = list
 
-class IconstPattern(Node):
-    def __init__(self, value):
-        self.value = value
-
-class FconstPattern(Node):
+class BconstPattern(Node):
     def __init__(self, value):
         self.value = value
 
@@ -198,14 +160,53 @@ class CconstPattern(Node):
     def __init__(self, value):
         self.value = value
 
-class SconstPattern(Node):
-    def __init__(self, value):
-        self.value = value
-
-class BconstPattern(Node):
+class FconstPattern(Node):
     def __init__(self, value):
         self.value = value
 
 class GenidPattern(Node):
     def __init__(self, name):
         self.name = name
+
+class IconstPattern(Node):
+    def __init__(self, value):
+        self.value = value
+
+class SconstPattern(Node):
+    def __init__(self, value):
+        self.value = value
+
+class NewExpression(Expression):
+    def __init__(self, type):
+        self.type = type
+
+class WhileExpression(Expression):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+class VariableDef(Def):
+    def __init__(self, name, type=None):
+        self.name = name
+        self.type = type
+
+class ArrayVariableDef(VariableDef):
+    def __init__(self, name, dimensions, type=None):
+        self.name = name
+        self.dimensions = dimensions
+        self.type = type
+
+class TypeDef(Node):
+    def __init__(self, list):
+        self.list = list
+
+class TDef(Node):
+    def __init__(self, name, list):
+        self.name = name
+        self.list = list
+
+class Constr(Node):
+    def __init__(self, name, list=None):
+        self.name = name
+        self.list = list or []
+
