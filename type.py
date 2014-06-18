2d71ea27ae076ba237597c2d16d4b09d7a8fe8d8
Nick Korasidis <renelvon@gmail.com>
2014-06-18 15:37:49 +0300

    Use error logging in type module. Fill docstrings.

--- type.py
+++ type.py
@@ -66,25 +66,58 @@ class Function(Type):
         self.toType = toType
 
 class TypeTable(Type):
+    """
+    Database of all the program's types. Enables semantic checking
+    of user defined types and more.
+    """
+
+    # Set of types encountered so far. Basic types always available.
     knownTypes = {'bool', 'int', 'float', 'char', 'unit'}
+
+    # Dictionary of constructors encountered so far.
+    # Each key contains a tuple (t, params), where:
+    #   t is the type to which the constructor belongs
+    #   params are the arguments of the constructor
     knownConstructors = {}
 
-    def __init__(self):
-        pass
+    # Logger used for logging events. Possibly shared with other modules.
+    _logger = None
+
+    def __init__(self, logger):
+        """Return a new TypeTable."""
+        self._logger = logger
 
     def process(self, typeDefList):
+        """
+        Analyse a user-defined type. Perform semantic checks
+        and insert type in the TypeTable.
+        """
+
+        # First, insert all newly-defined types.
         for newtype in typeDefList:
             if newtype.name in self.knownTypes:
-                print("Type reuse")
+                self._logger.error(
+                    #FIXME Add meaningful line
+                    "error: Type reuse"
+                    #TODO Show previous definition
+                )
             else:
                 self.knownTypes.add(newtype.name)
 
+        # Process each constructor.
         for tdef in typeDefList:
             for c in tdef:
                 if c.name in self.knownConstructors:
-                    print("Constructor reuse")
+                    self._logger.error(
+                        #FIXME add meaningful line
+                        "error: Constructor reuse"
+                        #TODO Show previous use
+                    )
                 else:
                     for t in c.list:
                         if t.name not in self.knownTypes:
-                            print("Type not defined")
+                            self._logger.error(
+                                #FIXME Add meaningful line
+                                "error: Type not defined"
+                            )
                     self.knownConstructors[c.name] = (tdef.name, c.list)
