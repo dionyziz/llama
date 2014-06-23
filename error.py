3c4781358009f6dd4f40c67223c3cd38ffa76be3
Nick Korasidis <renelvon@gmail.com>
2014-06-23 22:47:57 +0300

    Error: Align interfaces of Logger and LoggerMock.

--- error.py
+++ error.py
@@ -12,13 +12,16 @@ import logging
 
 
 class LoggerMock:
-    """Mock of a proper logger for testing purposes"""
-
-    def __init__(self):
-        pass
+    """
+    Interface and mock implementation of a full logger.
+    Mainly used for testing purposes.
+    """
 
     errors = 0
     warnings = 0
+
+    def __init__(self):
+        """Make a new mock loggger."""
 
     def debug(self, *args):
         pass
@@ -34,14 +37,19 @@ class LoggerMock:
 
     @property
     def success(self):
+        """Operation is successful iff zero errors are logged."""
         return self.errors == 0
 
     @property
     def perfect_success(self):
+        """
+        Operation is perfectly successful iff zero errors/warnings
+        are logged.
+        """
         return self.errors == 0 and self.warnings == 0
 
 
-class Logger:
+class Logger(LoggerMock):
     """
     Simple error logger for the llama compiler. Provides methods for
     logging and reporting errors of varying severities.
@@ -54,9 +62,6 @@ class Logger:
     # The logger instance, as constructed by the logging module
     _logger = None
 
-    errors = 0
-    warnings = 0
-
     def __init__(self, inputfile, level=logging.WARNING):
         """Create a new logger for the llama compiler."""
         self._logger = logging.getLogger('llama%d' % Logger._instances)
@@ -67,14 +72,15 @@ class Logger:
         handler.setFormatter(formatter)
         self._logger.addHandler(handler)
 
-    def critical(self, *args):
-        """Add a critical error to the logger."""
-        self._logger.critical(*args)
-
     def error(self, *args):
         """Add an error to the logger."""
         self._logger.error(*args)
         self.errors += 1
+
+    def warning(self, *args):
+        """Add a warning to the logger."""
+        self._logger.warning(*args)
+        self.warnings += 1
 
     def debug(self, *args):
         """Add some debug info to the logger."""
@@ -83,8 +89,3 @@ class Logger:
     def info(self, *args):
         """Add some general info to the logger."""
         self._logger.info(*args)
-
-    def warning(self, *args):
-        """Add a warning to the logger."""
-        self._logger.warning(*args)
-        self.warnings += 1
