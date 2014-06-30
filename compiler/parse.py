df260006ebdf2191f228ccddaace27ae0f122ecc
Nick Korasidis <renelvon@gmail.com>
2014-07-01 00:17:48 +0300

    Parser: Decorate AST/Type nodes with line info.
    Conflicts:
    	compiler/parse.py

--- compiler/parse.py
+++ compiler/parse.py
@@ -63,6 +63,7 @@ class Parser:
             p[0] = ast.LetDef(p[3], isRec=True)
         else:
             p[0] = ast.LetDef(p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_def_seq(self, p):
         """def_seq : def AND def_seq
@@ -81,6 +82,7 @@ class Parser:
             p[0] = ast.FunctionDef(p[1], p[2], p[6], p[4])
         else:
             p[0] = ast.FunctionDef(p[1], p[2], p[4])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_param_list(self, p):
         """param_list : param param_list
@@ -94,6 +96,7 @@ class Parser:
             p[0] = ast.Param(p[2], p[4])
         else:
             p[0] = ast.Param(p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_type(self, p):
         """type : LPAREN type RPAREN
@@ -111,6 +114,7 @@ class Parser:
                         | INT
                         | UNIT"""
         p[0] = type.builtin_map[p[1]]()
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_derived_type(self, p):
         """derived_type : array_type
@@ -126,6 +130,7 @@ class Parser:
             p[0] = type.Array(p[6], p[3])
         else:
             p[0] = type.Array(p[3])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_star_comma_seq(self, p):
         """star_comma_seq : TIMES COMMA star_comma_seq
@@ -139,14 +144,17 @@ class Parser:
     def p_function_type(self, p):
         """function_type : type ARROW type"""
         p[0] = type.Function(p[1], p[3])
+        p[0].set_pos(p[1])
 
     def p_ref_type(self, p):
         """ref_type : type REF"""
         p[0] = type.Ref(p[1])
+        p[0].set_pos(p[1])
 
     def p_user_type(self, p):
         """user_type : GENID"""
         p[0] = type.User(p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_empty(self, p):
         """empty :"""
@@ -194,18 +202,22 @@ class Parser:
                 | while_expr"""
         if len(p) == 4:
             p[0] = ast.BinaryExpression(p[1], p[2], p[3])
+            p[0].set_pos(p[1])
         elif len(p) == 3:
             p[0] = ast.UnaryExpression(p[1], p[2])
+            p[0].set_pos(p.lineno(1), p.lexpos(1))
         else:
             p[0] = p[1]
 
     def p_begin_end_expr(self, p):
         """begin_end_expr : BEGIN expr END"""
         p[0] = p[2]
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_ccall_expr(self, p):
         """ccall_expr : CONID simple_expr_seq"""
         p[0] = ast.ConstructorCallExpression(p[1], p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_simple_expr_seq(self, p):
         """simple_expr_seq : simple_expr simple_expr_seq
@@ -226,11 +238,13 @@ class Parser:
                        | uconst_simple_expr"""
         if len(p) == 5:
             p[0] = ast.ArrayExpression(p[1], p[3])
+            p[0].set_pos(p.lineno(1), p.lexpos(1))
         elif len(p) == 4:
             p[0] = p[2]
         elif len(p) == 3:
             # bang
             p[0] = ast.UnaryExpression(p[1], p[2])
+            p[0].set_pos(p.lineno(1), p.lexpos(1))
         else:
             p[0] = p[1]
 
@@ -238,38 +252,47 @@ class Parser:
         """bconst_simple_expr : TRUE
                               | FALSE"""
         p[0] = ast.ConstExpression(type.Bool(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_cconst_simple_expr(self, p):
         """cconst_simple_expr : CCONST"""
         p[0] = ast.ConstExpression(type.Char(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_conid_simple_expr(self, p):
         """conid_simple_expr : CONID"""
         p[0] = ast.ConidExpression(p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_iconst_simple_expr(self, p):
         """iconst_simple_expr : ICONST"""
         p[0] = ast.ConstExpression(type.Int(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_fconst_simple_expr(self, p):
         """fconst_simple_expr : FCONST"""
         p[0] = ast.ConstExpression(type.Float(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_genid_simple_expr(self, p):
         """genid_simple_expr : GENID"""
         p[0] = ast.GenidExpression(p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_sconst_simple_expr(self, p):
         """sconst_simple_expr : SCONST"""
         p[0] = ast.ConstExpression(type.String(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_uconst_simple_expr(self, p):
         """uconst_simple_expr : LPAREN RPAREN"""
         p[0] = ast.ConstExpression(type.Unit())
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_delete_expr(self, p):
         """delete_expr : DELETE expr"""
         p[0] = ast.DeleteExpression(p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_dim_expr(self, p):
         """dim_expr : DIM ICONST GENID
@@ -278,6 +301,7 @@ class Parser:
             p[0] = ast.DimExpression(p[3], p[2])
         else:
             p[0] = ast.DimExpression(p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_for_expr(self, p):
         """for_expr : FOR GENID EQ expr DOWNTO expr DO expr DONE
@@ -286,14 +310,17 @@ class Parser:
             p[0] = ast.ForExpression(p[2], p[4], p[6], p[8])
         else:
             p[0] = ast.ForExpression(p[2], p[4], p[6], p[8], isDown=True)
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_gcall_expr(self, p):
         """gcall_expr : GENID simple_expr_seq"""
         p[0] = ast.FunctionCallExpression(p[1], p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_in_expr(self, p):
         """in_expr : letdef IN expr"""
         p[0] = ast.LetInExpression(p[1], p[3])
+        p[0].set_pos(p[1])
 
     def p_if_expr(self, p):
         # WARNING: Changing order of clauses produces Syntax Errors,
@@ -304,10 +331,12 @@ class Parser:
             p[0] = ast.IfExpression(p[2], p[4], p[6])
         else:
             p[0] = ast.IfExpression(p[2], p[4])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_match_expr(self, p):
-        """match_expr : MATCH expr WITH clause_seq END """
+        """match_expr : MATCH expr WITH clause_seq END"""
         p[0] = ast.MatchExpression(p[2], p[4])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_clause_seq(self, p):
         """clause_seq : clause PIPE clause_seq
@@ -317,12 +346,14 @@ class Parser:
     def p_clause(self, p):
         """clause : pattern ARROW expr"""
         p[0] = ast.Clause(p[1], p[3])
+        p[0].set_pos(p[1])
 
     def p_pattern(self, p):
         """pattern : CONID simple_pattern_list
                    | simple_pattern"""
         if len(p) == 3:
             p[0] = ast.Pattern(p[1], p[2])
+            p[0].set_pos(p.lineno(1), p.lexpos(1))
         else:
             p[0] = p[1]
 
@@ -349,10 +380,12 @@ class Parser:
         """bconst_simple_pattern : TRUE
                                  | FALSE"""
         p[0] = ast.ConstExpression(type.Bool(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_cconst_simple_pattern(self, p):
         """cconst_simple_pattern : CCONST"""
         p[0] = ast.ConstExpression(type.Char(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_fconst_simple_pattern(self, p):
         """fconst_simple_pattern : FPLUS FCONST
@@ -361,14 +394,17 @@ class Parser:
             p[0] = ast.ConstExpression(type.Float(), p[2])
         else:
             p[0] = ast.ConstExpression(type.Float(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_mfconst_simple_pattern(self, p):
         """mfconst_simple_pattern : FMINUS FCONST"""
         p[0] = ast.ConstExpression(type.Float(), -p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_genid_simple_pattern(self, p):
         """genid_simple_pattern : GENID"""
         p[0] = ast.GenidPattern(p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_iconst_simple_pattern(self, p):
         """iconst_simple_pattern : PLUS ICONST
@@ -377,18 +413,22 @@ class Parser:
             p[0] = ast.ConstExpression(type.Int(), p[2])
         else:
             p[0] = ast.ConstExpression(type.Int(), p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_miconst_simple_pattern(self, p):
         """miconst_simple_pattern : MINUS ICONST"""
         p[0] = ast.ConstExpression(type.Int(), -p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_new_expr(self, p):
         """new_expr : NEW type"""
         p[0] = ast.NewExpression(p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_while_expr(self, p):
         """while_expr : WHILE expr DO expr DONE"""
         p[0] = ast.WhileExpression(p[2], p[4])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_variable_def(self, p):
         """variable_def : array_variable_def
@@ -402,6 +442,7 @@ class Parser:
             p[0] = ast.ArrayVariableDef(p[2], p[4], p[7])
         else:
             p[0] = ast.ArrayVariableDef(p[2], p[4])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_expr_comma_seq(self, p):
         """expr_comma_seq : expr COMMA expr_comma_seq
@@ -415,10 +456,12 @@ class Parser:
             p[0] = ast.VariableDef(p[2], p[4])
         else:
             p[0] = ast.VariableDef(p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_typedef(self, p):
         """typedef : TYPE tdef_and_seq"""
         p[0] = ast.TypeDefList(p[2])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
         self.typeTable.process(p[0])
 
     def p_tdef_and_seq(self, p):
@@ -429,6 +472,7 @@ class Parser:
     def p_tdef(self, p):
         """tdef : GENID EQ constr_pipe_seq"""
         p[0] = ast.TDef(p[1], p[3])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_constr_pipe_seq(self, p):
         """constr_pipe_seq : constr PIPE constr_pipe_seq
@@ -442,6 +486,7 @@ class Parser:
             p[0] = ast.Constructor(p[1], p[3])
         else:
             p[0] = ast.Constructor(p[1])
+        p[0].set_pos(p.lineno(1), p.lexpos(1))
 
     def p_type_seq(self, p):
         """type_seq : type type_seq
