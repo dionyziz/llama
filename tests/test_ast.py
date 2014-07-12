f264fe64c75c9b67aa8231b01a914258c0325739
Dionysis Zindros <dionyziz@gmail.com>
2014-07-12 15:55:32 +0300

    AST test: Regression test for __eq__ use of .type
    __eq__ in AST Nodes assumes two objects of the same class
    have the same attributes (no attributes are added in runtime).
    
    This is not true with the current type table, because it modifies
    ast.Constructor objects to add the `type` attribute. This regression
    test ensures equality works between ast.Constructor objects that
    are constructed manually and those that are passed through the type
    table.
    
    This is fixed in #25 and the regression test should pass.
    
    This is related to the fact that semantic checks are currently done
    before parsing is completed - this will change in future versions.

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -2,10 +2,48 @@ import unittest
 
 import sure
 
-from compiler import ast
+from compiler import ast, error, lex, parse
 
 
 class TestAST(unittest.TestCase):
+# TODO: remove boilerplate code after simple_api branch gets merged
+    def setUp(self):
+        self.parsers = {}
+
+    def _parse(self, data, start='program'):
+        mock = error.LoggerMock()
+
+        lexer = lex.Lexer(logger=mock, optimize=1)
+
+        # memoization
+        try:
+            parser = self.parsers[start]
+        except:
+            parser = self.parsers[start] = parse.Parser(
+                logger=mock,
+                optimize=0,
+                start=start,
+                debug=0
+            )
+
+        tree = parser.parse(
+            data=data,
+            lexer=lexer
+        )
+
+        return tree
+
     def test_eq(self):
         ast.Constructor("foo", []).should.be.equal(ast.Constructor("foo", []))
         ast.Constructor("foo", []).shouldnt.be.equal(ast.Constructor("bar", []))
+
+    def test_regression_attr_equality(self):
+        raise unittest.SkipTest("re-enable me after #25 gets merged")
+
+        tdef = self._parse("type color = Red", "typedef")
+        tdef2 = ast.TypeDefList([ast.TDef(ast.User("color"), [ast.Constructor("Red")])])
+
+        try:
+            tdef == tdef2
+        except:
+            self.assertTrue(False, "equality should not throw")
