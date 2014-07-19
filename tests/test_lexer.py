f16a0fb311640a4c4ae4570c417fdf7c8a49ca12
Nick Korasidis <renelvon@gmail.com>
2014-07-19 21:00:54 +0300

    Lex test: Expand testing of #8.

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -69,6 +69,7 @@ class TestLexer(unittest.TestCase):
     @unittest.skip("Enable me after bug #8 is fixed.")
     def test_nocolliding_ints_and_names(self):
         self._assert_lex_failure("42koko")
+        self._assert_lex_failure("42Koko")
 
     def test_conid(self):
         self._assert_individual_token("Koko", "CONID", "Koko")
