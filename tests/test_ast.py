fb7417acf47d673b60059859ab72b2604eaa37fd
Nikolaos Korasidis <renelvon@gmail.com>
2014-07-31 18:28:03 +0300

    Ast test: Style fixes.

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -23,55 +23,55 @@ class TestAST(unittest.TestCase, parser_db.ParserDB):
 
     def test_builtin_type_equality(self):
         for typecon in ast.builtin_types_map.values():
-            (typecon()).should.equal(typecon())
+            typecon().should.equal(typecon())
 
         for typecon1, typecon2 in itertools.combinations(
-                ast.builtin_types_map.values(), 2
+            ast.builtin_types_map.values(), 2
         ):
-            (typecon1()).shouldnt.equal(typecon2())
+            typecon1().shouldnt.equal(typecon2())
 
     def test_builtin_type_set(self):
         typeset = {typecon() for typecon in ast.builtin_types_map.values()}
         for typecon in ast.builtin_types_map.values():
-            (typeset).should.contain(typecon())
+            typeset.should.contain(typecon())
 
     def test_user_defined_types(self):
-        (ast.User("foo")).should.equal(ast.User("foo"))
+        ast.User("foo").should.equal(ast.User("foo"))
 
-        (ast.User("foo")).shouldnt.equal(ast.User("bar"))
-        (ast.User("foo")).shouldnt.equal(ast.Int())
+        ast.User("foo").shouldnt.equal(ast.User("bar"))
+        ast.User("foo").shouldnt.equal(ast.Int())
 
     def test_ref_types(self):
         footype = ast.User("foo")
         bartype = ast.User("bar")
         reffootype = ast.Ref(footype)
 
-        (reffootype).should.equal(ast.Ref(footype))
+        reffootype.should.equal(ast.Ref(footype))
 
-        (reffootype).shouldnt.equal(footype)
-        (reffootype).shouldnt.equal(ast.Ref(bartype))
+        reffootype.shouldnt.equal(footype)
+        reffootype.shouldnt.equal(ast.Ref(bartype))
 
     def test_array_types(self):
         inttype = ast.Int()
-        (ast.Array(inttype)).should.equal(ast.Array(inttype))
-        (ast.Array(inttype, 2)).should.equal(ast.Array(inttype, 2))
+        ast.Array(inttype).should.equal(ast.Array(inttype))
+        ast.Array(inttype, 2).should.equal(ast.Array(inttype, 2))
 
-        (ast.Array(ast.Int())).shouldnt.equal(ast.Array(ast.Float()))
-        (ast.Array(inttype, 1)).shouldnt.equal(ast.Array(inttype, 2))
+        ast.Array(ast.Int()).shouldnt.equal(ast.Array(ast.Float()))
+        ast.Array(inttype, 1).shouldnt.equal(ast.Array(inttype, 2))
 
         arr_int_type = ast.Array(inttype)
-        (arr_int_type).shouldnt.equal(inttype)
-        (arr_int_type).shouldnt.equal(ast.User("foo"))
-        (arr_int_type).shouldnt.equal(ast.Ref(inttype))
+        arr_int_type.shouldnt.equal(inttype)
+        arr_int_type.shouldnt.equal(ast.User("foo"))
+        arr_int_type.shouldnt.equal(ast.Ref(inttype))
 
     def test_function_types(self):
         intt = ast.Int()
-        (ast.Function(intt, intt)).should.equal(ast.Function(intt, intt))
+        ast.Function(intt, intt).should.equal(ast.Function(intt, intt))
 
         i2float = ast.Function(ast.Int(), ast.Float())
-        (i2float).shouldnt.equal(ast.Function(ast.Float(), ast.Int()))
+        i2float.shouldnt.equal(ast.Function(ast.Float(), ast.Int()))
 
-        (i2float).shouldnt.equal(intt)
-        (i2float).shouldnt.equal(ast.User("foo"))
-        (i2float).shouldnt.equal(ast.Ref(ast.Int()))
-        (i2float).shouldnt.equal(ast.Array(ast.Int()))
+        i2float.shouldnt.equal(intt)
+        i2float.shouldnt.equal(ast.User("foo"))
+        i2float.shouldnt.equal(ast.Ref(ast.Int()))
+        i2float.shouldnt.equal(ast.Array(ast.Int()))
