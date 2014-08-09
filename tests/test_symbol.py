0fbf250a502816c515543bedef60f912c1cd57f1
Nikolaos Korasidis <renelvon@gmail.com>
2014-08-09 23:28:32 +0300

    Symbol test: [TDD] Add exception for symbol error.

--- tests/test_symbol.py
+++ tests/test_symbol.py
@@ -1,6 +1,6 @@
 import unittest
 
-from compiler import error, symbol
+from compiler import ast, error, symbol
 
 
 class TestSymbolTableAPI(unittest.TestCase):
@@ -14,3 +14,14 @@ class TestSymbolTableAPI(unittest.TestCase):
         symbol_table2 = symbol.SymbolTable(logger=logger)
 
         symbol_table2.should.have.property("logger").being(logger)
+
+    @staticmethod
+    def test_redef_identifier_error():
+        node = ast.GenidExpression("foo")
+        node.lineno, node.lexpos = 1, 2
+        prev = ast.GenidExpression("foo")
+        prev.lineno, prev.lexpos = 3, 4
+        exc = symbol.RedefIdentifierError(node, prev)
+        exc.should.be.a(symbol.SymbolError)
+        exc.should.have.property("node").being(node)
+        exc.should.have.property("prev").being(prev)
