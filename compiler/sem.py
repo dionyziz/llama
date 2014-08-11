425f0dcbda5fc11d8badac6cd17cc4d1be38b689
Dionysis Zindros <dionyziz@gmail.com>
2014-08-11 19:21:45 +0300

    sem: Add complete working example of basic type checking

--- compiler/sem.py
+++ compiler/sem.py
@@ -1,16 +1,23 @@
-from compiler import ast, type
+from compiler import ast
+from compiler import type as tp
 
 
 class TypeChecker:
     def map_datanode(self, node):
+        print("Checking datanode of type " + type(node).__name__)
+        print("Declared type is " + node.type.__class__.__name__)
+
+        if node.type is None:
+            return
+
         try:
-            type.validate(node.type)
-        except InvalidTypeError as e:
-            ('%d:%d: error: Invalid type: ' + {
-                ArrayOfArrayError: 'Array of array',
-                ArrayReturnError: 'Function returning array',
-                RefOfArrayError: 'Reference of array'
-            }[type(e)]) % (e.node.lineno, e.node.lexpos)
+            tp.validate(node.type)
+        except tp.InvalidTypeError as e:
+            print(('%d:%d: error: Invalid type: ' + {
+                tp.ArrayOfArrayError: 'Array of array',
+                tp.ArrayReturnError: 'Function returning array',
+                tp.RefOfArrayError: 'Reference of array'
+            }[type(e)]) % (e.node.lineno, e.node.lexpos))
 
 
 def analyze(root):
